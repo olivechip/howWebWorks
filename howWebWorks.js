6a9956c916e8c4d6039a976b26031e8ee85c0047
Oliver <oliver@MSI>
2023-01-18 16:04:33 -0800

    completed exercises

---
+++ howWebWorks.js
@@ -0,0 +1,27 @@
+// Part One: Solidify Terminology
+// In your own terms, define the following terms:
+
+// What is HTTP?
+short for hypertext transfer protocol 
+it is a set of rules for how html renders and moves across networks
+// What is a URL?
+short for uniform resource locators
+it's what you type into a web browser to send requests for a specific resource
+// What is DNS?
+domain name system
+translates human readable text into ip addresses
+// What is a query string?
+specified text in a GET request to show results of a search
+// What are two HTTP verbs and how are they different?
+GET vs POST
+GET is used to request access to resources without changing data of server
+POST is used to request sending data over that changes server side info
+// What is an HTTP request?
+usually a request for specific info to server in the form of GET or POST 
+// What is an HTTP response?
+response that server sends to client when data was requested
+// What is an HTTP header? Give a couple examples of request and response headers you have seen.
+HTTP header specifies what and how data is rendered; eg: ACCEPT-LANGUAGE
+provide more info about request/response
+// What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+request is sent to server, server sends back response 2xx, 3xx, 4xx, etc.
